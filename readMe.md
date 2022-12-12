baa4296398eccc224aa58339b1524c5e06cfde20
Andriy000 <>
2022-12-12 14:53:29 +0200

    addReadme&Corrections

---
+++ readMe.md
@@ -0,0 +1,1 @@
+# it's my toDO project where i practice to fetch data from jsonplacehoder and then render this data in vanilla js
--- toDo.js
+++ toDo.js
@@ -4,36 +4,21 @@ fetch('https://jsonplaceholder.typicode.com/todos?_limit=10')
   .then((data)=>{
     console.log(data)
     for(let key in data){
-
       let content = document.getElementById("ToDOList");
-      
       const checkbox = document.createElement("input");
-   
       checkbox.type = "checkbox"
-      
-      
       ListEl =document.createElement('li');
       user = document.createElement('div');
       titleEl = document.createElement('div');
       completeEL = document.createElement('div');
-      
-   
-
       user.innerHTML = `User :${data[key].userId}`;
       titleEl.innerHTML =`${data[key].title}`
       completeEL.innerHTML =`${data[key].completed}`
-      
-
       content.appendChild(ListEl);
       ListEl.appendChild(user);
       ListEl.appendChild(titleEl);
       ListEl.appendChild(checkbox);
       ListEl.appendChild(completeEL)
-      
-      
-  
-     
-    
     }
     });
   
